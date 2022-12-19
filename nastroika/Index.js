a824288d0198393ba1778d155af0e7f1587350fc
Mark_Maistrenko <revanmayfall@gmail.com>
2022-12-19 20:11:42 +0400

    Create somthing difficuult

---
+++ nastroika/Index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+// настраиваем приложение , реагировать на запрос
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/samurais', (req, res) => {
+  res.send('Hello samurais!')
+})
+// post запрос 
+app.post('/samurais', (req, res) => {
+  res.send('We have created samurai!')
+})
+
+
+// слушай порт и извести по старту программы 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+

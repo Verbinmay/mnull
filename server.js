7bda7633063e213f39845096654f3b3c12ad7703
Verbinmay <revanmayfall@gmail.com>
2022-12-19 11:46:22 +0400

    rr

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const fs = require ('fs')
-const server = http.createServer((request, response)=> {
-    switch(reques.url){
-        case '/home': {
-            const data = 'best free online course'
-            response.write(data)
-            response.end()
-            break;
-        }
-        default:{
-            response.write('404 not found')
-            response.end()
-        }
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+  switch (request.url) {
+    case "/home": {
+      const data = "best free online course"
+      response.write(data)
+      response.end()
+      break;
     }
-})
+    default: {
+      response.write("404 not found")
+      response.end()
+    }
+  }
+});
+
 server.listen(3003)
+

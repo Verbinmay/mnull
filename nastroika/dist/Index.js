8fb47a24cff82ce76ea027fd9707da4977aa7600
Mark_Maistrenko <revanmayfall@gmail.com>
2022-12-20 16:29:16 +0400

    add filters 1)fetch('http://localhost:3000/courses?title=end', {method: 'GET'}) 2)fetch('http://localhost:3000/courses/3', {method: 'GET'})

--- nastroika/dist/Index.js
+++ nastroika/dist/Index.js
@@ -6,23 +6,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get("/courses", (req, res) => {
-    res.json([
-        { id: 1, title: "Front-End" },
-        { id: 2, title: "Back-End" },
+const db = {
+    courses: [
+        { id: 1, title: "front-end" },
+        { id: 2, title: "back-end" },
         { id: 3, title: "JS" },
         { id: 4, title: "CSS" },
-    ]);
+    ],
+};
+app.get("/courses", (req, res) => {
+    let foundCourse = db.courses;
+    //это все курсы, но если вдруг есть есть в куери тайтл, то фильтруй
+    if (req.query.title) {
+        foundCourse = foundCourse.filter((c) => c.title.indexOf(req.query.title) > -1);
+    }
+    /*
+  Так вот , чтобы обработать вясяки фильтры со ???, надо использовать в рекуесте куери as sring
+  
+  fetch('http://localhost:3000/courses?title=end', {method: 'GET'})
+  .then(res => res.json())
+  .then(json => console.log(json))*/
+    //!length -нет длинны
+    if (!foundCourse.length) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundCourse);
 });
 app.get("/courses/:id", (req, res) => {
-    const foundCourse = [
-        { id: 1, title: "Front-End" },
-        { id: 2, title: "Back-End" },
-        { id: 3, title: "JS" },
-        { id: 4, title: "CSS" },
-        //Интересная хуйня, мы переводим параметр знаком из строки в число +
-        // req это то, что мы получаем, посмотреть надо в params 
-    ].find(c => c.id === +req.params.id);
+    const foundCourse = db.courses.find((c) => c.id === +req.params.id);
+    /*Интересная хуйня, мы переводим параметр знаком из строки в число +
+      req это то, что мы получаем, посмотреть надо в params
+  
+      fetch('http://localhost:3000/courses/3', {method: 'GET'})
+  .then(res => res.json())
+  .then(json => console.log(json))
+      */
     if (!foundCourse) {
         res.sendStatus(404);
         return;
--- nastroika/src/Index.ts
+++ nastroika/src/Index.ts
@@ -1,28 +1,51 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
-
-app.get("/courses", (req, res) => {
-  res.json([
-    { id: 1, title: "Front-End" },
-    { id: 2, title: "Back-End" },
+const db = {
+  courses: [
+    { id: 1, title: "front-end" },
+    { id: 2, title: "back-end" },
     { id: 3, title: "JS" },
     { id: 4, title: "CSS" },
-  ]);
+  ],
+};
+
+app.get("/courses", (req, res) => {
+  let foundCourse = db.courses;
+  //это все курсы, но если вдруг есть есть в куери тайтл, то фильтруй
+  if (req.query.title) {
+    foundCourse = foundCourse.filter(
+      (c) => c.title.indexOf(req.query.title as string) > -1
+    );
+  }
+  /* 
+Так вот , чтобы обработать вясяки фильтры со ???, надо использовать в рекуесте куери as sring
+
+fetch('http://localhost:3000/courses?title=end', {method: 'GET'})
+.then(res => res.json())
+.then(json => console.log(json))*/
+
+  //!length -нет длинны
+  if (!foundCourse.length) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.json(foundCourse);
 });
 
 app.get("/courses/:id", (req, res) => {
-  const foundCourse = [
-    { id: 1, title: "Front-End" },
-    { id: 2, title: "Back-End" },
-    { id: 3, title: "JS" },
-    { id: 4, title: "CSS" },
-    //Интересная хуйня, мы переводим параметр знаком из строки в число +
-    // req это то, что мы получаем, посмотреть надо в params 
-  ].find(c => c.id === +req.params.id);
+  const foundCourse = db.courses.find((c) => c.id === +req.params.id);
+  /*Интересная хуйня, мы переводим параметр знаком из строки в число + 
+    req это то, что мы получаем, посмотреть надо в params 
 
-  if(!foundCourse){
+    fetch('http://localhost:3000/courses/3', {method: 'GET'})
+.then(res => res.json())
+.then(json => console.log(json))
+    */
+
+  if (!foundCourse) {
     res.sendStatus(404);
     return;
   }
@@ -31,4 +54,4 @@ app.get("/courses/:id", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});

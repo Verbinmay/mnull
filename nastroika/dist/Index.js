fdf9528037144d08ca925f5954074668849bcb12
Mark_Maistrenko <revanmayfall@gmail.com>
2022-12-20 15:30:19 +0400

    bred

--- nastroika/dist/Index.js
+++ nastroika/dist/Index.js
@@ -6,23 +6,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-// настраиваем приложение , реагировать на запрос
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/courses", (req, res) => {
+    res.json([
+        { id: 1, title: "Front-End" },
+        { id: 2, title: "Back-End" },
+        { id: 3, title: "JS" },
+        { id: 4, title: "CSS" },
+    ]);
 });
-app.get('/samurais', (req, res) => {
-    const a = 4;
-    if (a > 5) {
-        res.send('OK');
+app.get("/courses/:id", (req, res) => {
+    const foundCourse = [
+        { id: 1, title: "Front-End" },
+        { id: 2, title: "Back-End" },
+        { id: 3, title: "JS" },
+        { id: 4, title: "CSS" },
+        //Интересная хуйня, мы переводим параметр знаком из строки в число +
+        // req это то, что мы получаем, посмотреть надо в params 
+    ].find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
     }
-    else {
-        res.send('Hello Word!');
-    }
+    res.json(foundCourse);
 });
-app.post('/samurais', (req, res) => {
-    res.send('Hih');
-});
-// слушай порт и извести по старту программы 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });

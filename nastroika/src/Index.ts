586be220d45ecc0a449bbb083c031c32c0febb6f
Mark_Maistrenko <revanmayfall@gmail.com>
2022-12-22 21:25:32 +0400

    +tipizaciya

--- nastroika/src/Index.ts
+++ nastroika/src/Index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { RequestWithQuery } from "./types";
 
 export const app = express();
 const port = 3000;
@@ -21,7 +22,13 @@ export const HTTP_STATUSES = {
   BAD_REQUEST_400: 400,
   NOT_FOUND_404: 404,
 };
-const db = {
+
+type CourseType = {
+  id: number;
+  title: string;
+};
+
+const db: { courses: CourseType[] } = {
   courses: [
     { id: 1, title: "front-end" },
     { id: 2, title: "back-end" },
@@ -30,30 +37,34 @@ const db = {
   ],
 };
 
-app.get("/courses", (req, res) => {
-  let foundCourse = db.courses;
-  //это все курсы, но если вдруг есть есть в куери тайтл, то фильтруй
-  if (req.query.title) {
-    foundCourse = foundCourse.filter(
-      (c) => c.title.indexOf(req.query.title as string) > -1
-    );
-  }
-  /* 
+app.get(
+  "/courses",
+  (req: RequestWithQuery<{ title: string }>, res: Response<CourseType[]>) => {
+    let foundCourse = db.courses;
+    //это все курсы, но если вдруг есть есть в куери тайтл, то фильтруй
+    if (req.query.title) {
+      foundCourse = foundCourse.filter(
+        (c) => c.title.indexOf(req.query.title) > -1
+      );
+    }
+    /* 
 Так вот , чтобы обработать вясяки фильтры со ???, надо использовать в рекуесте куери as sring
 
 fetch('http://localhost:3000/courses?title=end', {method: 'GET'})
 .then(res => res.json())
 .then(json => console.log(json))*/
 
-  //!length -нет длинны
- /* if (!foundCourse.length) {
+    //!length -нет длинны
+    /* if (!foundCourse.length) {
     res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
     return;
   }*/
-  res.json(foundCourse);
-});
+    res.json(foundCourse);
+  }
+);
 
-app.get("/courses/:id", (req, res) => {
+// query parametr always string
+app.get("/courses/:id", (req: Request<{ id: string }>, res) => {
   const foundCourse = db.courses.find((c) => c.id === +req.params.id);
   /*Интересная хуйня, мы переводим параметр знаком из строки в число + 
     req это то, что мы получаем, посмотреть надо в params 
@@ -67,34 +78,37 @@ app.get("/courses/:id", (req, res) => {
     res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
     return;
   }
- 
+
   res.json(foundCourse);
 });
 
-app.post("/courses", (req, res) => {
-  //добавим проверку на наличие
-  if (!req.body.title) {
-    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
-    return;
-  }
+app.post(
+  "/courses",
+  (req: Request<{}, {}, { title: string }>, res: Response<CourseType>) => {
+    //добавим проверку на наличие
+    if (!req.body.title) {
+      res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+      return;
+    }
 
-  const createdCourse = {
-    id: +new Date(),
-    title: req.body.title,
-  };
-  /* опять хреначим из даты число */
-  db.courses.push(createdCourse);
-  res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
-  //статус креатед будет видно в нетворке в хеадерс
-  /*
+    const createdCourse = {
+      id: +new Date(),
+      title: req.body.title,
+    };
+    /* опять хреначим из даты число */
+    db.courses.push(createdCourse);
+    res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
+    //статус креатед будет видно в нетворке в хеадерс
+    /*
   fetch('http://localhost:3000/courses', {method: 'POST', body : JSON.stringify({title:'dba'}), headers :{
   'content-type' : 'application/json'}})
   .then(res => res.json())
   .then(json => console.log(json))
 */
-});
+  }
+);
 
-app.delete("/courses/:id", (req, res) => {
+app.delete("/courses/:id", (req: Request<{ id: string }>, res) => {
   db.courses = db.courses.filter((c) => c.id !== +req.params.id);
   /*
   fetch('http://localhost:3000/courses/1', {method: 'DELETE'})
@@ -106,22 +120,24 @@ app.delete("/courses/:id", (req, res) => {
   res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 });
 
-app.put("/courses/:id", (req, res) => {
-  if (!req.body.title) {
-    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
-    return;
-  }
+app.put(
+  "/courses/:id",
+  (req: Request<{ id: string }, {}, { title: string }>, res) => {
+    if (!req.body.title) {
+      res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+      return;
+    }
 
-  const foundCourse = db.courses.find((c) => c.id === +req.params.id);
+    const foundCourse = db.courses.find((c) => c.id === +req.params.id);
 
-  if (!foundCourse) {
-    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-    return;
-  }
+    if (!foundCourse) {
+      res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+      return;
+    }
 
-  foundCourse.title = req.body.title;
+    foundCourse.title = req.body.title;
 
-  /* 
+    /* 
 ругается, но делает 
 
 fetch('http://localhost:3000/courses/1', {method: 'PUT', body : JSON.stringify({title:'dba'}), headers :{
@@ -131,8 +147,9 @@ fetch('http://localhost:3000/courses/1', {method: 'PUT', body : JSON.stringify({
 .then(json => console.log(json))
 */
 
-  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-});
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+  }
+);
 
 app.delete("/__test__/data", (req, res) => {
   db.courses = [];
